5d513269c8822b17f154ebba6d05bdde1e5c38ba
stosempreingiro <122582919+stosempreingiro@users.noreply.github.com>
2024-04-26 18:28:15 +0200

    Update NetINFO.py

--- NetINFO.py
+++ NetINFO.py
@@ -10,24 +10,24 @@ else:
     clear="clear"
 
 def subnet_decimal(ip_user,info=None):
-    list_info=ip_user.split("/")  #divido l'input dell'utente in indirizzo IP e subnet
+    list_info=ip_user.split("/")  #I split the user input in IP and Subnet
     ip=list_info[0].split(".")
     sub=list_info[1]
-    x=32-int(sub)
-    sub_net_bin=""              #Creo la subnet in binario
+    x=32-int(sub)     #number of zeros for the binary subnet
+    sub_net_bin=""              #Empty variable that will contain the subnet in binary 
     n=0
     for i in range (0,int(sub)):
         sub_net_bin+='1'
-        if len(sub_net_bin)==8 or len(sub_net_bin)==17 or len(sub_net_bin)==26: #condizione che permettere di inserire i punti.
+        if len(sub_net_bin)==8 or len(sub_net_bin)==17 or len(sub_net_bin)==26: #Condition that allows you to insert dots.
             sub_net_bin+='.'
     for i in range(0,x):
         sub_net_bin+='0'
         if len(sub_net_bin)==8 or len(sub_net_bin)==17 or len(sub_net_bin)==26:
             sub_net_bin+='.'
-    listina_binari=sub_net_bin.split(".")   #creo una lista con i singoli valori della subnet in binario
-    subnet_decimal=""       #creo subnet in decimale
+    listina_binari=sub_net_bin.split(".")   #I create a list of 4 elements, each of which will be made up of 8 of the 32 bits of the subnet
+    subnet_decimal=""   
     punto=0
-    for numero in listina_binari:
+    for numero in listina_binari:   #Start creating a subnet in decimal
         if punto!=3:
             numero=int(numero,2)
             numero=str(numero)
@@ -37,13 +37,13 @@ def subnet_decimal(ip_user,info=None):
             numero=int(numero,2)
             numero=str(numero)
             subnet_decimal+=numero
-    ip_bin=""               #riscrivo l'indirizzo IP in binario.
+    ip_bin=""               #Start writing IP in binary
     for val in ip:
         bin_val=bin(int(val))[2:].zfill(8)
         ip_bin+=bin_val
         if len(ip_bin)==8 or len(ip_bin)==17 or len(ip_bin)==26:
             ip_bin+='.'
-    gateway_bin=""       #Calcolo e scrivo il gateway e l'host min in binario
+    gateway_bin=""       #start calculating the gateway and the net_addr (host_min) in binary
     host_min=""
     for i in range(0,len(sub_net_bin)):
         if sub_net_bin[i]=='.':
@@ -58,7 +58,7 @@ def subnet_decimal(ip_user,info=None):
             host_min+=ip_bin[i]
     list_gateway_bin=gateway_bin.split(".")
     list_host_min=host_min.split(".")
-    gateway_decimal=""      #calcolo e scrivo il gateway e il broadcast in decimale
+    gateway_decimal=""      #Start Calculating Gateway and Broadcast in decimal
     broad_cast=""
     i=0
     for val in list_gateway_bin:
@@ -72,7 +72,7 @@ def subnet_decimal(ip_user,info=None):
             gateway_decimal+='.'
             broad_cast+='.'
 
-    host_min_decimal=""     #scrivo l'host min in decimale
+    host_min_decimal=""     #Calculating Net Addr in decimal
     i=0
     for val in list_host_min:
         i+=1
@@ -80,7 +80,9 @@ def subnet_decimal(ip_user,info=None):
         host_min_decimal+=str(val_dec)
         if i!=4:
             host_min_decimal+='.'
-    if info=="solo_sub":        #valori di ritorno
+
+     #Manage the return value of the function
+    if info=="solo_sub":       
         return subnet_decimal
     elif info=="tutto":
         return f"-SUBNET: {subnet_decimal}\n-HOST MIN/NETADDRESS: {host_min_decimal}\n-BROADCAST/HOST MAX: {broad_cast}\n-Possible GATEWAY: {gateway_decimal}\n-INDIRIZZI IP TOTALI nella RETE: {2**(32-int(sub))}\n-DEVICE CHE POSSONO ESSERE CONNESSI: {(2**(32-int(sub)))-2}"
@@ -93,8 +95,9 @@ def subnet_decimal(ip_user,info=None):
                 only_ip+=carattere
         return f"{only_ip} {subnet_decimal}"     
     
-def check_input_ip(ip_user):        #funzione che verifica l'input dell'utente
+def check_input_ip(ip_user):        #Verify user input
     try:
+        ip_user=ip_user.strip()
         punti=0
         val=True
         for char in ip_user:
@@ -145,8 +148,8 @@ try:
             continue
         subnet_gateway_out=subnet_decimal(ip_user,info="tutto")
         print(f"\n{subnet_gateway_out}\n")
-        z=input("(Invio per proseguire, CTRL+C per interrompere)")
-        if z=="":
+        to_do=input("(Invio per proseguire, CTRL+C per interrompere)")
+        if to_do=="":
             os.system(clear)
 except KeyboardInterrupt:
     print("\nByebye\n")
